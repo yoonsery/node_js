45619ea0eb90999f3e1228e4cafbb77dd58ad5c6
yoonsery <yoon.sery@icloud.com>
2021-04-26 22:02:13 +0900

    add id for update

--- main.js
+++ main.js
@@ -80,11 +80,11 @@ const app = http.createServer(function (request, response) {
       const template = templateHTML(
         title,
         list,
-        `<form action="http://localhost:3000/create_process" method="post">
-          <p><input type="text" name="title" placeholder="title"/></p>
+        `<form action="/create_process" method="post">
+          <p><input type="text" name="title" placeholder="title"></p>
           <p><textarea name="description" placeholder="description"></textarea></p>
           <p><input type="submit" /></p>
-        </form`,
+        </form>`,
         ''
       );
       response.writeHead(200);
@@ -102,6 +102,29 @@ const app = http.createServer(function (request, response) {
       fs.writeFile(`data/${title}`, description, 'utf8', (err) => {
         response.writeHead(302, { Location: `/?id=${title}` });
         response.end();
+      });
+    });
+  } else if (pathname === '/update') {
+    fs.readdir('./data', function (error, filelist) {
+      fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
+        const title = queryData.id;
+        const list = templateList(filelist);
+        const template = templateHTML(
+          title,
+          list,
+          `
+          <form action="/update_process" method="post">
+            <input type="hidden" name="id" value="${title}">
+            <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+            <p><textarea name="description"   placeholder="description">${description}</textarea></p>
+            <p><input type="submit" /></p>
+          </form>
+          `,
+          `<a href="/create">create</a>
+            <a href="/update?id=${title}">update</a>`
+        );
+        response.writeHead(200);
+        response.end(template);
       });
     });
   } else {
